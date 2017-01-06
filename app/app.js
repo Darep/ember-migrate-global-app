c581ded8f71b2268cbab6322b76895a31efe2d2b
Antti-Jussi Kovalainen <ajk@ajk.fi>
2017-01-06 12:03:46 +0200

    Add the custom resolver and module wrapper

--- app/app.js
+++ app/app.js
@@ -1,7 +1,28 @@
+/* global loader */
 import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+
+loader.wrapModules = function (name, callback) {
+  const callbackAsString = callback.toString();
+  const appModules = callbackAsString.match(/App\.[^ ]+/);
+  const noExports = callbackAsString.match(/exports\[/) === null;
+  const appModuleName = appModules && appModules.length ? appModules[0] : null;
+
+  if (noExports && appModuleName) {
+    return function (exports) {
+      callback(exports);
+
+      // Fetch the window.App.FooBar module
+      var module = appModuleName.split('.').reduce((memo, part) => memo = memo[part], window);
+
+      exports["default"] = module;
+    };
+  } else {
+    return callback;
+  }
+};
 
 let App;
 
--- app/resolver.js
+++ app/resolver.js
@@ -1,3 +1,32 @@
+/* global requirejs */
 import Resolver from 'ember-resolver';
+import ModuleRegistry from 'ember-resolver/utils/module-registry';
 
-export default Resolver;
+function DasherizedModuleRegistry(entries) {
+  var keys;
+
+  this._entries = entries || requirejs.entries;
+
+  // Convert underscore in module names to dashes
+  keys = Object.keys(this._entries);
+  keys.forEach((key) => {
+    var dasherizedKey = key.replace(/_/g, '-');
+    if (dasherizedKey !== key) {
+      this._entries[dasherizedKey] = this._entries[key];
+      delete this._entries[key];
+    }
+  });
+}
+
+DasherizedModuleRegistry.prototype = Object.create(ModuleRegistry.prototype);
+DasherizedModuleRegistry.prototype.constructor = DasherizedModuleRegistry;
+
+export default Resolver.extend({
+  init: function() {
+    this._super();
+
+    if (!this._moduleRegistry || !(this._moduleRegistry instanceof DasherizedModuleRegistry)) {
+      this._moduleRegistry = new DasherizedModuleRegistry();
+    }
+  },
+});
